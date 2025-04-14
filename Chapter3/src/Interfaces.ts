7f2ae75014dda53072b0210aa078fcefbf123314
Krishanu Mishra <krishanu.mishra@intimetec.com>
2025-04-14 13:01:54 +0530

    Changed for it to use classes

--- Chapter3/src/Interfaces.ts
+++ Chapter3/src/Interfaces.ts
@@ -1,21 +1,21 @@
-export interface UserInput {
+export interface IUserInput {
     blogName: string,
     startPost: number,
     endPost: number
 };
 
-export interface TumblrPost {
+export interface ITumblrPost {
     [key: string]: any;
     "photo-url-1280"?: string;
     "photo-url-500"?: string;
 }
 
-export interface TumblrData {
+export interface ITumblrData {
     tumblelog: {
         title: string;
         name: string;
         description: string;
     };
     "posts-total": number;
-    posts: TumblrPost[];
+    posts: ITumblrPost[];
 }
